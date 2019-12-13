af7c82fb0a78ff898d6df8d2b35ccfca949d707f
concefly <concefly@foxmail.com>
2019-12-13 17:56:32 +0800

    fix: type

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import Loadable from 'react-loadable';
 
 /** 异步加载依赖 */
-export function withLazyInjectProps<P = any>(provider: () => Promise<P>, loading: () => any) {
+export function withLazyInjectProps<P = any>(
+  provider: () => Promise<P>,
+  loading: () => any
+) {
   return function<C>(Comp: C): C {
     return Loadable({
       loader: async () => {
@@ -16,7 +19,8 @@ export function withLazyInjectProps<P = any>(provider: () => Promise<P>, loading
 
           return React.createElement(Comp as any, newProps);
         };
-        const name = Comp.displayName || Comp.name;
+        const name = (Comp as any).displayName || (Comp as any).name;
+
         refFunc.displayName = `withLazyInjectProps(${name})`;
         const LazyComp = React.forwardRef<any, P>(refFunc);
 
